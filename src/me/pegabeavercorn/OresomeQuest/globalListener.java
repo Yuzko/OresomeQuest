54d542c74ced470e3982cef38670032e60f89bcd
Yuzko <ow_oh_kiss_liss3456@hotmail.com>
2013-08-18 12:45:06 +1000

    Update globalListener.java

--- src/me/pegabeavercorn/OresomeQuest/globalListener.java
+++ src/me/pegabeavercorn/OresomeQuest/globalListener.java
@@ -1,80 +1,67 @@
-package me.pegabeavercorn.OresomeQuest;
+package me.yuzko.quest;
 
 import org.bukkit.Location;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
+public class globalListener implements Listener
+{
+	Quest plugin;
 
+	public globalListener(Quest pl)
+	{
+		plugin = pl;
+	}
 
-public class globalListener implements Listener{
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void join(PlayerJoinEvent event)
+	{
+		Player player = event.getPlayer();
+		String name = player.getName();
 
-	oresomequest plugin;
-	
-	public globalListener(oresomequest pl){
-		plugin = pl;}
-	
-	
-	
-
-	@EventHandler(priority =  EventPriority.NORMAL)
-	public void join(PlayerJoinEvent event){
-		
-		Player p = event.getPlayer();
-		String name = p.getName();
-		
 		Location spawn = new Location(plugin.getServer().getWorld("world"), 639, 68, 1072);
-		
-		p.teleport(spawn);
-		
+
+		player.teleport(spawn);
+
 		if(plugin.pl.getConfig().get(name) == null ){
-			
-			plugin.pl.getConfig().set(name + ".Char1.Name", null);
-			plugin.pl.getConfig().set(name + ".Char1.Level", null);
-			plugin.pl.getConfig().set(name + ".Char1.Progression", null);
-			plugin.pl.getConfig().set(name + ".Char1.Path", null);
-			plugin.pl.getConfig().set(name + ".Char1.Preffered", null);
-			plugin.pl.getConfig().set(name + ".Char1.Skills", null);
-			plugin.pl.getConfig().set(name + ".Char1.LastLocation.x", null);
-			plugin.pl.getConfig().set(name + ".Char1.LastLocation.y", null);
-			plugin.pl.getConfig().set(name + ".Char1.LastLocation.z", null);
-			plugin.pl.getConfig().set(name + ".Char1.LastLocation.world", null);
-			
-	        plugin.pl.saveConfig();
-	        plugin.pl.reloadConfig();
-			
+			{
+				plugin.pl.getConfig().set(name + "Char1.Name", null);
+				plugin.pl.getConfig().set(name + ".Char1.Level", null);
+				plugin.pl.getConfig().set(name + ".Char1.Progression", null);
+				plugin.pl.getConfig().set(name + ".Char1.Path", null);
+				plugin.pl.getConfig().set(name + ".Char1.Preffered", null);
+				plugin.pl.getConfig().set(name + ".Char1.Skills", null);
+				plugin.pl.getConfig().set(name + ".Char1.LastLocation.x", null);
+				plugin.pl.getConfig().set(name + ".Char1.LastLocation.y", null);
+				plugin.pl.getConfig().set(name + ".Char1.LastLocation.z", null);
+				plugin.pl.getConfig().set(name + ".Char1.LastLocation.world", null);
+				plugin.pl.saveConfig();
+				plugin.pl.reloadConfig();
+			}
 		}
-		
-
-		
-			
-		}
-
-	@EventHandler(priority =  EventPriority.NORMAL)
-	public void quit(PlayerQuitEvent event){
-		
-		Player p = event.getPlayer();
-		String name = p.getName();
-		Location loc = p.getLocation();
-		int x = loc.getBlockX();
-		int y = loc.getBlockY();
-		int z = loc.getBlockZ();
-		String world = loc.getWorld().getName();
+	}
+	
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void quit(PlayerQuitEvent event)
+	{
+		Player player = event.getPlayer();
+		String name = player.getName();
+		Location location = player.getLocation();
+		int x = location.getBlockX();
+		int y = location.getBlockY();
+		int z = location.getBlockZ();
+		String world = location.getWorld().getName();
 		
 		plugin.pl.getConfig().set(name + ".Char1.LastLocation.x", x);
 		plugin.pl.getConfig().set(name + ".Char1.LastLocation.y", y);
 		plugin.pl.getConfig().set(name + ".Char1.LastLocation.z", z);
 		plugin.pl.getConfig().set(name + ".Char1.LastLocation.world", world);
+		plugin.pl.saveConfig();
+		plugin.pl.reloadConfig();
 		
-        plugin.pl.saveConfig();
-        plugin.pl.reloadConfig();
 	}
-	
-	}
-	
-
+}
