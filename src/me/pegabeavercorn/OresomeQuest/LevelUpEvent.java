3c35c2aedb76fdb5a5d4b0c58b4836000d8128f7
Yuzko <ow_oh_kiss_liss3456@hotmail.com>
2013-08-18 12:43:37 +1000

    Update LevelUpEvent.java

--- src/me/pegabeavercorn/OresomeQuest/LevelUpEvent.java
+++ src/me/pegabeavercorn/OresomeQuest/LevelUpEvent.java
@@ -1,63 +1,65 @@
-package me.pegabeavercorn.OresomeQuest;
+package me.yuzko.quest;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
-public class LevelUpEvent extends Event {
-	private static final HandlerList handlers = new HandlerList();
+public class LevelUpEvent extends Event
+{
+	public static final HandlerList handlers = new HandlerList();
 	private Player player;
 	int level = 0;
-	
-	oresomequest plugin;
-	
-	public LevelUpEvent(oresomequest pl){
-		plugin = pl;}
-	
-	
-	
-	public LevelUpEvent(Player p){
+
+	Quest plugin;
+
+
+	public LevelUpEvent(Quest pl)
+	{
+	plugin = pl;
+	}
+
+	public LevelUpEvent(Player p)
+	{
 	player = p;
-		
 	}
 
-public Player getPlayer(){
-	
-	return player;
-}
-	
-	
-public int getLevel(){
-
-	String name = player.getName();
-
-	
-	if(plugin.pl.getConfig().get(name) != null ){
-		
-		if(plugin.pl.getConfig().get(name + ".Level") != null ){
-	int level = plugin.pl.getConfig().getInt(name + ".Level"); 	
-		
-	return level;
-	}else{
-		return 0;
-	}
-	}else{
-		return 0;
+	public LevelUpEvent(int i) {
+		// TODO Auto-generated constructor stub
 	}
 
-	
-	
-	
-}
+	public Player getPlayer()
+	{
+		return player;
+	}
 
+	public int getLevel()
+	{
+		String name = player.getName();
+		if(plugin.pl.getConfig().get(name) != null)
+		{
+			if(plugin.pl.getConfig().get(name + ".Level") != null)
+			{
+				int level = plugin.pl.getConfig().getInt(name + ".Level");
+				return level;
+			}
+			else
+			{
+				return 0;
+			}
+		}
+		else
+		{
+			return 0;
+		}
+	}
 
-
-	
-	public HandlerList getHandlers() {
+	public HandlerList getHandlers()
+	{
 		return handlers;
 	}
-	
-    public static HandlerList getHandlerList() {
-        return handlers;
-    }
+
+	public static HandlerList getHandlerList()
+	{
+		return handlers;
+	}
 }
